a667050a3b9eb20fa555ae92a5efca9073f65a52
HAN BYEONG JUN <75424986+appoung@users.noreply.github.com>
2024-12-06 06:41:11 +0000

    fileupload

--- app.py
+++ app.py
@@ -1,6 +1,14 @@
+import os
 from flask import Flask, request, jsonify, render_template
 from werkzeug.utils import secure_filename
+
 app = Flask(__name__)
+
+# Make sure the upload folder exists
+UPLOAD_FOLDER = './uploads'
+if not os.path.exists(UPLOAD_FOLDER):
+    os.makedirs(UPLOAD_FOLDER)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 # 1. 기본 경로에 대한 라우트
 
@@ -30,12 +38,15 @@ def omgwtf():
     return "스타코딩의 짱은 한병준이다"
 
 
-@app.route('/fileupload', methods=['GET'])
+@app.route('/fileupload', methods=['GET', 'POST'])
 def file_upload():
-    file = request.files['file']
-    filename = secure_filename(file.filename)
-    file.save(os.path.join('./uploads', filename))
-    return 'Upload 성공'
+    if request.method == 'POST':
+        file = request.files['file']
+        if file:
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return 'Upload 성공'
+    return render_template('fileupload.html')
 
 
 if __name__ == '__main__':
